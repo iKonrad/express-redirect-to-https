bb2eebb8e55501884a181315a5e3be3ee6f3ffef
Konrad Jarosinski <konrad@jarosinski.uk>
2021-05-24 21:51:10 +0200

    Fixes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 let httpRedirectServer = express();
 
 // set up a route to redirect http to https
@@ -5,7 +7,8 @@ httpRedirectServer.get('*', (request, response) => {
   response.redirect('https://' + request.headers.host + request.url);
 });
 
-httpRedirectServer.listen(process.env.PORT || 3001);
-httpRedirectServer.on('listening', () => {
-  console.log("Listening to redirect http to https");
+const port = process.env.PORT || 3001;
+
+httpRedirectServer.listen(port, () => {
+  console.log("Listening to redirect http to https on PORT: ", port);
 });
